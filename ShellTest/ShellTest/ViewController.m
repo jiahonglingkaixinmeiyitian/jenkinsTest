02245c0ee27199c14e51240a4dbc85089c3dc2d0
heartjhl <2858796570@qq.com>
2018-10-25 19:28:38 +0800

    添加lable

--- ShellTest/ShellTest/ViewController.m
+++ ShellTest/ShellTest/ViewController.m
@@ -18,6 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor redColor];
+    UILabel *ji = [[UILabel alloc]init];
+    [self.view addSubview:ji];
+    ji.frame = CGRectMake(100, 100, 100, 60);
+    ji.backgroundColor = [UIColor blueColor];
+    ji.text = @"第一次";
 }
 
 
